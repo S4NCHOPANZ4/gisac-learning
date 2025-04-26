a59a3234721d78fdece42fc60d6f7cbd98686701
Juan Buitrago <buitr4go@gmail.com>
2025-04-25 20:10:32 -0500

    added .env file

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://gisac-learning.vercel.app/",
     credentials: true,
   }));
   
